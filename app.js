b212cadabf5190d9f6d69f2727ea3b2f6ed94954
Ron B <me@ronthecookie.me>
2020-01-13 16:53:27 +0200

    add null case

--- app.js
+++ app.js
@@ -4,24 +4,29 @@ const app = express();
 const ytdl = require("ytdl-core");
 const port = process.env.PORT || process.env.port || 3000;
 const ffmpeg = require("fluent-ffmpeg");
-const yt_regex = /^(http(s)?:\/\/)((w){3}.)?youtu(be|.be)?(\.com)?\/watch\?v=[a-zA-Z0-9]+/
+const yt_regex = /^(http(s)?:\/\/)((w){3}.)?youtu(be|.be)?(\.com)?\/watch\?v=[a-zA-Z0-9]+/;
 
 app.use(express.static("static"));
 
 app.get("/api", async (req, res) => {
-    const {url, type} = req.query;
+    const { url, type } = req.query;
     if (!["mp3", "mp4"].includes(type)) return res.sendStatus(400);
     if (!yt_regex.test(url)) return res.sendStatus(400);
     const info = await ytdl.getBasicInfo(url);
     console.log(info);
-    res.setHeader("Content-Disposition", `attachment; filename=${info.title.split(" ").join("_")}.${type}`);
+    res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=${(info.title || "")
+            .split(" ")
+            .join("_")}.${type}`
+    );
     if (type == "mp4") {
         ytdl(url).pipe(res);
     } else {
         ffmpeg(ytdl(url))
             .format("mp3")
             .audioBitrate(128)
-            .pipe(res)
+            .pipe(res);
     }
 });
 
